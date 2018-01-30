fdc364a20bcd75088d465e44768d793cb47865dd
Seth <s.zim517@gmail.com>
2018-01-29 19:16:28 -0500

    Modifies color on devicon icons in project section

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
 $('.nav a').on('click', (event) => {
     $('.nav').find('.active').removeClass('active');
     $(event.target).parent().addClass('active');
+    $(".navbar-collapse").collapse('hide');
+});
+
+// Transitions to different parts of page
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop':  $target.offset().top
+    }, 700, 'swing', function () {
+        window.location.hash = target;
+    });
 });
